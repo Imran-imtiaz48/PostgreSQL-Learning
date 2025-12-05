e8336ddad4bdf74d2e90d9c867c1bac059fb661b
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2025-12-05 09:04:08 +0400

    Refactor SQL commands for person2 and person4
    Updated SQL commands for altering person2 and added a select statement for person4.

--- alter.sql
+++ alter.sql
@@ -1,40 +1,46 @@
 -- Active: 1699467625194@@127.0.0.1@5432@ph@public
 
+-- Select all data from person4
+SELECT * 
+FROM person4;
 
-SELECT * from person4;
+-- --- Alter person2 table examples ---
 
+-- Add a new column with default value and NOT NULL constraint
 ALTER TABLE person2
-    ADD COLUMN email VARCHAR(25)  DEFAULT 'default@mail.com' NOT NULL;
+ADD COLUMN email VARCHAR(25) NOT NULL DEFAULT 'default@mail.com';
 
+-- Drop a column
 ALTER TABLE person2
-    DROP COLUMN email;
+DROP COLUMN email;
 
+-- Rename a column
 ALTER TABLE person2
-    RENAME COLUMN age to user_age;
+RENAME COLUMN age TO user_age;
 
+-- Change column data type
 ALTER TABLE person2
-    ALTER COLUMN user_name TYPE VARCHAR(50);
+ALTER COLUMN user_name TYPE VARCHAR(50);
 
+-- Add a UNIQUE constraint to a column
 ALTER TABLE person2
-    ALTER COLUMN user_age set UNIQUE;
+ADD CONSTRAINT unique_user_age UNIQUE(user_age);
 
+-- Drop NOT NULL constraint
 ALTER TABLE person2
-    ALTER COLUMN user_age drop NOT null;
+ALTER COLUMN user_age DROP NOT NULL;
 
-
+-- Drop UNIQUE constraint
 ALTER TABLE person2
-    ADD constraint unique_person2_user_age UNIQUE(user_age);
+DROP CONSTRAINT unique_user_age;
 
+-- --- Data management operations ---
 
-ALTER TABLE person2
-    DROP constraint unique_person2_user_age;
+-- Remove all data from table (resets sequences in PostgreSQL if any)
+TRUNCATE TABLE person2 RESTART IDENTITY;
 
-
-TRUNCATE TABLE person2;
+-- Drop the table completely
 DROP TABLE person2;
 
-
--- INSERT into person4 VALUES(10,'mizan', 45);
-
-
-
+-- --- Example insert statement ---
+-- INSERT INTO person4(id, name, age) VALUES (10, 'mizan', 45);
